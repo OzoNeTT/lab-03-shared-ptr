e790022347c9656dc2bd8e5e1afc910a475fd28a
Alexey Gorbachev <Alexey1020@gmail.com>
2019-09-10 21:43:06 +0300

    Complete ZaSharel_ptr

--- include/Shared.hpp
+++ include/Shared.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <atomic>
 #include <iostream>
+#include <algorithm>
 #include <cstddef>
+#include <string>
 
 using namespace std;
 
@@ -15,20 +17,110 @@ public:
         pointer = nullptr;
         totalUse = nullptr;
     }
-    SharedPtr(const SharedPtr& r);
-    SharedPtr(SharedPtr&& r);
-    auto opeartor=(const SharedPtr& r) -> SharedPtr&;
-    auto opeartor=(SharedPtr&& r) -> SharedPtr&;
+    explicit SharedPtr(T *ptr) {
+        pointer = ptr;
+        totalUse = new atomic_uint{1};
+    }
+
+    SharedPtr(const SharedPtr& r) {
+        totalUse = nullptr;
+        *this = r;
+    }
+
+    SharedPtr(SharedPtr&& r) {
+        totalUse = nullptr;
+        *this = std::forward<SharedPtr>(r);
+    }
+    auto operator=(const SharedPtr& r) -> SharedPtr& {
+        if (this == &r)
+            return *this;
+        if (totalUse != nullptr) {
+            --(*totalUse);
+            if (*totalUse == 0) {
+                delete pointer;
+                delete totalUse;
+            }
+        }
+        pointer = r.pointer; totalUse = r.totalUse;
+        (*totalUse)++;
+        return *this;
+
+    }
+    auto operator=(SharedPtr&& r) -> SharedPtr&{
+        if (this == &r)
+            return *this;
+        if (totalUse != nullptr) {
+            --(*totalUse);
+            if (*totalUse == 0) {
+                delete pointer;
+                delete totalUse;
+            }
+        }
+        pointer = r.pointer; totalUse = r.totalUse;
+        r.pointer = nullptr; r.totalUse = nullptr;
+    }
 
     // проверяет, указывает ли указатель на объект
-    operator bool() const;
-    auto operator*() const -> T&;
-    auto operator->() const -> T*;
+    operator bool() const {
+        if (pointer != nullptr)
+            return true;
+        return false;
+    }
 
-    auto get() -> T*;
-    void reset();
-    void reset(T* ptr);
-    void swap(SharedPtr& r);
+    auto operator*() const -> T&{
+        return *pointer;
+    }
+
+    auto operator->() const -> T* {
+        return pointer;
+    }
+
+    auto get() -> T*{
+        return pointer;
+    }
+
+    void reset() {
+        if (totalUse != nullptr) {
+            --(*totalUse);
+            if (*totalUse == 0) {
+                delete pointer;
+                delete totalUse;
+            }
+            pointer = nullptr;
+            totalUse = nullptr;
+        }
+    }
+
+    void reset(T* ptr){
+        if (totalUse != nullptr) {
+            --(*totalUse);
+            if (*totalUse == 0) {
+                delete pointer;
+                delete totalUse;
+            }
+        }
+        pointer = ptr;
+        totalUse = new SharedPtr{1};
+        totalUse++;
+    }
+
+    void swap(SharedPtr& r) {
+        std::swap(pointer, r.pointer);
+        std::swap(totalUse, r.totalUse);
+    }
+
     // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
-    auto use_count() const -> size_t;
+    auto use_count() const -> size_t {
+        return *totalUse;
+    }
+    ~SharedPtr() {
+        if( totalUse == nullptr)
+            return;
+        --(*totalUse);
+        if (*totalUse == 0) {
+            delete pointer;
+            delete totalUse;
+        }
+    }
+
 };
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,16 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include "../include/Shared.hpp"
 
-#include <Shared.hpp>
+
+TEST(SharedPtr, FromPointer)
+{
+    auto sourcePtr = new int{789};
+    auto ptr = SharedPtr<int>{sourcePtr};
+
+    EXPECT_EQ(static_cast<bool>(ptr), true);
+    EXPECT_EQ(ptr.use_count(), 1u);
+    EXPECT_EQ(ptr.get(), sourcePtr);
+    EXPECT_EQ(*ptr, 789);
+}
